f9aee61189af8aa85dce40104d8f0e9797e52ba3
r2abreu <r2abreu@pm.me>
2020-12-01 12:22:02 +0100

    Termino de ejercicio

--- JS/JS03 - Variables/ejercicio02/index.js
+++ JS/JS03 - Variables/ejercicio02/index.js
@@ -3,9 +3,9 @@ const parragraphs = document.querySelectorAll('p');
 const radius = document.getElementById('radius');
 
 const calculate = () => {
-	parragraphs[0].firstElementChild.textContent = 2 * PI * radius.value;
-	parragraphs[1].firstElementChild.textContent = PI * radius.value ** 2;
-	parragraphs[2].firstElementChild.textContent = 2 * radius.value;
+	parragraphs[0].firstElementChild.textContent = Math.round(2 * PI * radius.value);
+	parragraphs[1].firstElementChild.textContent = Math.round(PI * radius.value ** 2);
+	parragraphs[2].firstElementChild.textContent = Math.round(2 * radius.value);
 };
 
 radius.addEventListener('keyup', function(event) {
