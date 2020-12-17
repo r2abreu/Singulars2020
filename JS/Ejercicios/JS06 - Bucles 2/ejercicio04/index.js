91fcdf4cae454aa7265b954370972bdf8ff42a69
r2abreu <r2abreu@pm.me>
2020-12-17 13:26:32 +0100

    Mejora de ejercicio

--- JS/Ejercicios/JS06 - Bucles 2/ejercicio04/index.js
+++ JS/Ejercicios/JS06 - Bucles 2/ejercicio04/index.js
@@ -8,14 +8,18 @@ globalThis.addEventListener('DOMContentLoaded', function() {
 const showResult = () => {
 	let number = parseInt(document.querySelector('#number').value);
 	let isPrime = true;
-
-	for (let i = 2; i < number; i++) {
-		console.log(i);
-		if (number % i === 0) {
-			isPrime = false;
-			break;
-		}
-	}
 	let output = document.querySelector('output');
-	output.textContent = isPrime ? 'Primo.' : 'No primo.';
+	console.log(number === 1);
+	if (number === 1) {
+		output.textContent = 'El número 1 no es primo ni compuesto.';
+	} else {
+		for (let i = 2; i < number; i++) {
+			console.log(i);
+			if (number % i === 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		output.textContent = isPrime ? 'Primo.' : 'No primo.';
+	}
 };
