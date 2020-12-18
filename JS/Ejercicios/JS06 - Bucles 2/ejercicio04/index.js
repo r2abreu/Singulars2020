5dae2d6bb4e27eacc840ed6b2afe56d27fcdde17
r2abreu <r2abreu@pm.me>
2020-12-18 12:43:04 +0100

    Termino de ejercicio

--- JS/Ejercicios/JS06 - Bucles 2/ejercicio04/index.js
+++ JS/Ejercicios/JS06 - Bucles 2/ejercicio04/index.js
@@ -9,12 +9,10 @@ const showResult = () => {
 	let number = parseInt(document.querySelector('#number').value);
 	let isPrime = true;
 	let output = document.querySelector('output');
-	console.log(number === 1);
 	if (number === 1) {
 		output.textContent = 'El número 1 no es primo ni compuesto.';
 	} else {
 		for (let i = 2; i < number; i++) {
-			console.log(i);
 			if (number % i === 0) {
 				isPrime = false;
 				break;
