a166841e3f42d522d988d8646a17dc9b0c26f255
r2abreu <r2abreu@pm.me>
2020-12-28 16:47:39 +0100

    Implementacion de TS

---
+++ JS/Ejercicios/JS08 - Funciones 2/ejercicio01/index.ts
@@ -0,0 +1,61 @@
+// IIFE process, obtains elements and register event listener
+
+
+(function() {
+    let image = document.querySelector('img');
+    let title = document.querySelector('output');
+    title.textContent = "Luna Nueva"
+    image.addEventListener('click', function() {
+        nextItem(image, title)
+    })
+})()
+
+// Switch statement to change img properties based on the ALT
+
+const nextItem = (image, title) => {
+    switch(image.alt) {
+        case "Luna Nueva": 
+            image.src = "./assets/crescent-moon.webp";
+            image.alt = "Luna Cresciente";
+            title.textContent = image.alt;
+            break;
+        case "Luna Cresciente":
+            image.src = "./assets/first-quarter.webp";
+            image.alt = "Cuarto Cresciente";
+            title.textContent = image.alt;
+            break;
+        case "Cuarto Cresciente":
+            image.src = "./assets/waxing-gibbous.png";
+            image.alt = "Creciente Gibosa";
+            title.textContent = image.alt;
+            break;
+        case "Creciente Gibosa":
+            image.src = "./assets/full-moon.png";
+            image.alt = "Luna Llena";
+            title.textContent = image.alt;
+            break;
+        case "Luna Llena": 
+            image.src = "./assets/wanning-gibbous.png";
+            image.alt = "Menguante Gibosa";
+            title.textContent = image.alt;
+            break;
+        case "Menguante Gibosa":
+            image.src = "./assets/third-quarter.png";
+            image.alt = "Cuarto Menguante";
+            title.textContent = image.alt;
+            break;
+        case "Cuarto Menguante":
+            image.src =  "./assets/wanning-crescent.png";
+            image.alt = "Menguante";
+            title.textContent = image.alt;
+            break;
+        case "Menguante": 
+            image.src =  "./assets/new-moon.png";
+            image.alt = "Luna Nueva";
+            title.textContent = image.alt;
+            break;       
+    }
+}
+
+
+// END
