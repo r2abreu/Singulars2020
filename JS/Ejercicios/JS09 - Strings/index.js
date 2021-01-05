c15575a9305d5c4dc58ec04979c90a124a637915
r2abreu <r2abreu@pm.me>
2021-01-05 09:46:35 +0100

    Termino de ejercicio

--- JS/Ejercicios/JS09 - Strings/index.js
+++ JS/Ejercicios/JS09 - Strings/index.js
@@ -13,6 +13,15 @@ function handleClick() {
 	outputs[1].textContent = string.concat(' - Miguel De Cervantes');
 	outputs[2].textContent = string.replace('fermosura', 'hermosura');
 	outputs[3].textContent = string.indexOf('razón');
+	outputs[4].textContent = string.lastIndexOf('razón');
+	// La diferencia entre indexOf y search es que Search te permite especificar regExp, mientras que indexOf solo permite especificar texto plano
+	outputs[5].textContent = string.search('razón');
+	outputs[6].textContent = string.slice(0, 10);
+	outputs[7].textContent = JSON.stringify(string.split(' '));
+	outputs[8].textContent = string.substring(0, 10);
+	outputs[9].textContent = string.toLowerCase();
+	outputs[10].textContent = string.toUpperCase();
+	outputs[11].textContent = string.trim();
 
 	outputs.forEach((output) => output.classList.add('output'));
 }
