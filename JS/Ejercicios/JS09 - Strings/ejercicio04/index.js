3731adf996a1b97f976e2dc1631dc2aea4dcf7c8
r2abreu <r2abreu@pm.me>
2021-01-05 11:30:55 +0100

    Termino de ejercicio

---
+++ JS/Ejercicios/JS09 - Strings/ejercicio04/index.js
@@ -0,0 +1,41 @@
+// IIFE
+
+(function() {
+	let buttons = document.querySelectorAll('button');
+	buttons.forEach((button) => {
+		button.addEventListener('click', handleClick);
+	});
+})();
+
+function handleClick() {
+	let string = document.querySelector('[type="text"]').value;
+	let key = parseInt(document.querySelector('[type="number"]').value);
+
+	this.textContent === 'Cifrar' ? encryptString(string, key, 'cypher') : encryptString(string, key, 'decypher');
+}
+
+function encryptString(str, key, operation) {
+	let encryptedString = '';
+	for (let i = 0; i < str.length; i++) {
+		if (operation === 'cypher') {
+			encryptedString += String.fromCharCode(str.charCodeAt(i) + key);
+		} else {
+			encryptedString += String.fromCharCode(str.charCodeAt(i) - key);
+		}
+	}
+
+	printResult(reverseStr(encryptedString));
+}
+
+function printResult(result) {
+	let output = document.querySelector('output');
+	output.textContent = result;
+}
+
+function reverseStr(str) {
+	let newStr = '';
+	for (let i = 1; i <= str.length; i++) {
+		newStr += `${str[str.length - i]}`;
+	}
+	return newStr;
+}
