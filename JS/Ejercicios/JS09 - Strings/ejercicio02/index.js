487d5b82d6a980bf8e828ed15faf9b322c202929
r2abreu <r2abreu@pm.me>
2021-01-05 10:18:12 +0100

    Fin de ejercicio

---
+++ JS/Ejercicios/JS09 - Strings/ejercicio02/index.js
@@ -0,0 +1,30 @@
+// IIFE
+
+(function() {
+	let button = document.querySelector('button');
+	button.addEventListener('click', handleClick);
+})();
+
+function handleClick() {
+	let name = document.querySelector('#name').value;
+	let lastname1 = document.querySelector('#lastname1').value;
+	let lastname2 = document.querySelector('#lastname2').value;
+
+	transformStrings(name, lastname1, lastname2);
+}
+
+function printResult(name, lastname1, lastname2) {
+	let output = document.querySelector('output');
+	output.textContent = `${lastname1} ${lastname2}, ${name}`;
+}
+
+function transformStrings(name, lastname1, lastname2) {
+	let firstChar = name.slice(0, 1).toUpperCase();
+	let restOfSentence = name.slice(1).toLowerCase();
+
+	let transformedName = `${firstChar}${restOfSentence}`;
+	let transformedLastname1 = lastname1.toUpperCase();
+	let transformedLastname2 = lastname2.toUpperCase();
+
+	printResult(transformedName, transformedLastname1, transformedLastname2);
+}
