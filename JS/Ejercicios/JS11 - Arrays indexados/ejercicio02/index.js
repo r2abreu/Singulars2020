23267482248352a0eb0b22fb33b8df7cacd320bb
r2abreu <r2abreu@pm.me>
2021-01-08 11:59:19 +0100

    Termino de ejercicio

---
+++ JS/Ejercicios/JS11 - Arrays indexados/ejercicio02/index.js
@@ -0,0 +1,17 @@
+// IIFE
+
+(function() {
+	let button = document.querySelector('button');
+	button.addEventListener('click', handleClick);
+})();
+
+function handleClick() {
+	let output = document.querySelector('output');
+	output.textContent = generateArray();
+}
+
+function generateArray() {
+	let newArr = [];
+	for (let i = 1; i <= 1000; i++) newArr.push(7 * i);
+	return JSON.stringify(newArr);
+}
