1f9b611c1e9341aafc92ff3688e1cf3bad6e3f58
r2abreu <r2abreu@pm.me>
2021-01-18 18:11:42 +0100

    Termino ddel ejercicio

---
+++ JS/Ejercicios/JS13 - Dates/ejercicio01/index.js
@@ -0,0 +1,20 @@
+(() => {
+    let button = document.querySelector('button');
+    button ? button.addEventListener('click', handleClick) : null;
+})();
+function handleClick() {
+    let output = document.querySelectorAll('output');
+    // Create date object
+    let date = new Date();
+    output[0].textContent = JSON.stringify(date);
+    // getDate()
+    output[1].textContent = String(date.getDate());
+    // getDay()
+    output[2].textContent = String(date.getDay());
+    // getFullyear()
+    output[3].textContent = String(date.getFullYear());
+    // getHours()
+    output[4].textContent = String(date.getHours());
+    // getMiliseconds()
+    output[5].textContent = String(date.getMilliseconds());
+}
