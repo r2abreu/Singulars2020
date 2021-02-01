1243fe2b6faeadc1edac402d1467b0659e0838c4
r2abreu <r2abreu@pm.me>
2021-02-01 20:19:43 +0100

    Termino de ejercicios

--- JS/Ejercicios/JS19 - DOM3/ejercicio04/index.js
+++ JS/Ejercicios/JS19 - DOM3/ejercicio04/index.js
@@ -1,47 +1,102 @@
-
+// Create game class
 
 let game = class {
-    constructor(stats) {
-        this.tokens = stats.tokens;
-        this.plays = stats.plays;
-        this.win = stats.win;
-        this.loss = stats.loss;
-        this.record = stats.record;
-    }
+	constructor(stats) {
+		this.tokens = stats.tokens;
+		this.plays = stats.plays;
+		this.win = stats.win;
+		this.loss = stats.loss;
+		this.record = stats.record;
+	}
 
-    randomDice() {
-        return Math.floor(Math.random() * 6) + 1;
-    }
-}
+	randomDice() {
+		return Math.floor(Math.random() * 6) + 1;
+	}
+};
+
+// Stats object
 
 const stats = {
-    tokens: 10,
-    plays: 0, 
-    win: 0, 
-    loss:0,
-    record: 0,
-}
+	tokens: 10,
+	plays: 0,
+	win: 0,
+	loss: 0,
+	record: 10
+};
+
+// Create a new game
 
 const newGame = new game(stats);
 
+// Display stats
+
+function display() {
+	let outputs = document.querySelectorAll('output');
+	outputs[0].textContent = newGame.tokens;
+	outputs[1].textContent = newGame.plays;
+	outputs[2].textContent = newGame.win;
+	outputs[3].textContent = newGame.loss;
+	outputs[4].textContent = newGame.record;
+}
+
+// Add listeners for the form
 
 (() => {
-    let button = document.querySelector('button');
-    button.addEventListener('click', () => {
-        handlePlay(newGame);
-    });
+	display();
+	let form = document.querySelector('form');
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();
+
+		// Check if user has tokens to play, if not, throw error
+
+		newGame.tokens <= 0 ? throwError('No tienes mas monedas, vuelve a empezar') : handlePlay(newGame);
+	});
 })();
 
-
-
+// Handle play
 
 function handlePlay(game) {
-    let dice1 = game.randomDice();
-    let dice2 = game.randomDice();
-    let playResult = dice1 + dice2;
-    let bet = parseInt(document.querySelector('[type="number"]').value);
+	let images = document.querySelectorAll('img');
+	let dice1 = game.randomDice();
+	let dice2 = game.randomDice();
+	let radios = document.querySelectorAll('[type="radio"]');
 
-    playResult > 7 ? gameWon() : gameLost();
+	images[0].src = `./assets/${dice1}.svg`;
+	images[1].src = `./assets/${dice2}.svg`;
 
-    console.log(game, playResult)
+	const tier = [ ...radios ].find((radio) => radio.checked);
+
+	let playResult = dice1 + dice2;
+	let bet = parseInt(document.querySelector('[type="number"]').value);
+
+	game.plays++;
+
+	if (tier.value === 'below') {
+		playResult < 7 ? gameWon(game, bet) : gameLost(game, bet);
+	} else {
+		playResult > 7 ? gameWon(game, bet) : gameLost(game, bet);
+	}
+
+	display();
+}
+
+// Handle loss
+
+function gameLost(game, bet) {
+	game.loss++;
+	game.tokens -= bet;
+	game.tokens <= 0 ? alert('Te has quedado sin monedas, vuelve a empezar') : null;
+}
+
+// Handle win
+
+function gameWon(game, bet) {
+	game.win++;
+	game.tokens += bet;
+	game.record = game.tokens > game.record ? game.tokens : game.record;
+	console.log(bet);
+}
+
+function throwError(str) {
+	alert(`${str}`);
 }
--- JS/Ejercicios/JS20 - BOM/ejercicio03/index.js
+++ JS/Ejercicios/JS20 - BOM/ejercicio03/index.js
@@ -1,8 +1,12 @@
 let img = document.querySelector('img');
 let routes = [ 'image1.jpeg', 'image2.jpeg', 'image3.jpeg', 'image4.jpg' ];
 
-routes.forEach((route, index) => {
-	setTimeout(() => {
-		img.src = `./assets/${route}`;
-	}, index * 1000);
-});
+setInterval(() => {
+	routes.forEach((route, index) => {
+		setTimeout(() => {
+			img.src = `./assets/${route}`;
+			img.alt = `Imagen ${index}`;
+			img.title = `Imagen ${index}`;
+		}, index * 1000);
+	});
+}, 4000);
